85041634ae0d4ec627839da39069576e6ab70fb2
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-02-13 16:56:42 +0100

    Trying to fix windows build

--- build/build.cmd
+++ build/build.cmd
@@ -1,12 +1,12 @@
 cd %~dp0
 set CONDA_BLD_PATH=%cd%\..\output
 
-call:cb .\descartes || exit /B 1
-call:cb .\geopandas || exit /B 1
-call:cb .\motionless || exit /B 1
-call:cb .\salem || exit /B 1
-call:cb .\cleo || exit /B 1
-call:cb .\oggm || exit /B 1
+call:cb %* .\descartes || exit /B 1
+call:cb %* .\geopandas || exit /B 1
+call:cb %* .\motionless || exit /B 1
+call:cb %* .\salem || exit /B 1
+call:cb %* .\cleo || exit /B 1
+call:cb %* .\oggm || exit /B 1
 
 pause
 exit /B %ERRORLEVEL%
