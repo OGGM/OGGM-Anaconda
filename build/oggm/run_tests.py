f55ac75749b47a8599f48560b507137fca8d4e7b
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-02-13 14:09:40 +0100

    oggm testing

---
+++ build/oggm/run_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import nose
+import os
+import oggm
+import sys
+
+initial_dir = os.getcwd()
+oggm_file = os.path.abspath(oggm.__file__)
+oggm_dir = os.path.dirname(oggm_file)
+os.chdir(oggm_dir)
+
+nose_argv = sys.argv
+nose_argv += ['--detailed-errors', '--exe']
+
+nose.main(argv=nose_argv)
+
+
