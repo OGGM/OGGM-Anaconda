000f3e6ba73c0e52ba308a116395e81622129c03
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-03-17 16:06:52 +0100

    Parallelize builds

--- build/build.sh
+++ build/build.sh
@@ -3,8 +3,12 @@ cd "$(dirname "$0")"
 export CONDA_BLD_PATH="$PWD/../output"
 
 function cb() {
-	conda build --no-anaconda-upload --python 3.5 --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
-	conda build --no-anaconda-upload --python 2.7 --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
+	if [ -z "${CONDA_BUILD_PY}" ]; then
+		conda build --no-anaconda-upload --python 3.5 --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
+		conda build --no-anaconda-upload --python 2.7 --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
+	else
+		conda build --no-anaconda-upload --python "${CONDA_BUILD_PY}" --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
+	fi
 }
 
 cb ./descartes
