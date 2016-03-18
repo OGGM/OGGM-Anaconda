9141d5b0888be9014038f44f8dd1bb39fca5562c
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-03-18 10:52:54 +0100

    Use Agg backend for testing

--- build/oggm/run_test.py
+++ build/oggm/run_test.py
@@ -5,6 +5,9 @@ import os
 import oggm
 import sys
 
+import matplotlib
+matplotlib.use('Agg')
+
 initial_dir = os.getcwd()
 oggm_file = os.path.abspath(oggm.__file__)
 oggm_dir = os.path.dirname(oggm_file)
