71d0f005976c79b0c019a3b82b8954671745e62f
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-04-05 13:20:08 +0200

    Use conda-forge instead of ioos

--- build/build.sh
+++ build/build.sh
@@ -4,10 +4,10 @@ export CONDA_BLD_PATH="$PWD/../output"
 
 function cb() {
 	if [ -z "${CONDA_BUILD_PY}" ]; then
-		conda build --no-anaconda-upload --python 3.5 --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
-		conda build --no-anaconda-upload --python 2.7 --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
+		conda build --no-anaconda-upload --python 3.5 --channel defaults --channel conda-forge --channel oggm --override-channels "$@" || exit -1
+		conda build --no-anaconda-upload --python 2.7 --channel defaults --channel conda-forge --channel oggm --override-channels "$@" || exit -1
 	else
-		conda build --no-anaconda-upload --python "${CONDA_BUILD_PY}" --channel defaults --channel ioos --channel oggm --override-channels "$@" || exit -1
+		conda build --no-anaconda-upload --python "${CONDA_BUILD_PY}" --channel defaults --channel conda-forge --channel oggm --override-channels "$@" || exit -1
 	fi
 }
 
