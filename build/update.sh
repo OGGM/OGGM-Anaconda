c7b4deda88ce7c17aaab81aedc94f89ba6920611
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-11-17 15:53:26 +0100

    No need to update motionless anymore

--- build/update.sh
+++ build/update.sh
@@ -6,7 +6,6 @@ function getSha() {
 
 cd "$(dirname "$0")"
 
-SHA_MOTIONLESS="$(getSha fmaussion/motionless)"
 SHA_SALEM="$(getSha fmaussion/salem)"
 SHA_CLEO="$(getSha fmaussion/cleo)"
 SHA_OGGM="$(getSha OGGM/oggm)"
@@ -15,14 +14,12 @@ sed -i -r "s|(url: .*/).*|\1${SHA_SALEM}|" salem/meta.yaml || exit -2
 sed -i -r "s|(fn: .*-).*(\.tar\.gz)|\1${SHA_SALEM}\2|" salem/meta.yaml || exit -2
 sed -i -r "s|(url: .*/).*|\1${SHA_CLEO}|" cleo/meta.yaml || exit -2
 sed -i -r "s|(fn: .*-).*(\.tar\.gz)|\1${SHA_CLEO}\2|" cleo/meta.yaml || exit -2
-sed -i -r "s|(url: .*/).*|\1${SHA_MOTIONLESS}|" motionless/meta.yaml || exit -2
-sed -i -r "s|(fn: .*-).*(\.tar\.gz)|\1${SHA_MOTIONLESS}\2|" motionless/meta.yaml || exit -2
 sed -i -r "s|(url: .*/).*|\1${SHA_OGGM}|" oggm/meta.yaml || exit -2
 sed -i -r "s|(fn: .*-).*(\.tar\.gz)|\1${SHA_OGGM}\2|" oggm/meta.yaml || exit -2
 
 DATE_STR="$(date +%Y%m%d%H%M)"
 
-for i in salem cleo motionless oggm; do
+for i in salem cleo oggm; do
 	if ! git diff --quiet --exit-code "${i}"/meta.yaml; then
 		sed -i -r "s|(version: .*\.).*|\1${DATE_STR}\"|" "${i}"/meta.yaml || exit -2
 	fi
