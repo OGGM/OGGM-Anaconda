4e32c6d351e6f24d1a41572c1dd6bd61746b6444
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2017-01-19 11:48:59 +0100

    Update OGGM

--- build/oggm/run_test.py
+++ build/oggm/run_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import nose
+import pytest
 import os
 import oggm
 import sys
@@ -11,16 +11,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 import matplotlib
 matplotlib.use('agg')
 
-# os.environ["OGGM_SLOW_TESTS"] = "True";
-
 initial_dir = os.getcwd()
 oggm_file = os.path.abspath(oggm.__file__)
 oggm_dir = os.path.dirname(oggm_file)
-os.chdir(oggm_dir)
 
-nose_argv = sys.argv
-nose_argv += ['--detailed-errors', '--exe']
-
-nose.main(argv=nose_argv)
-
-
+sys.exit(pytest.main([oggm_dir, '--mpl']))
