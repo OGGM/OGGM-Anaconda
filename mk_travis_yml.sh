96ba6c0a4f71a2b18435e4ca6a9705949f9379ef
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2017-08-10 17:15:49 +0200

    Increase timeout for conda build

--- mk_travis_yml.sh
+++ mk_travis_yml.sh
@@ -10,7 +10,7 @@ for stage in motionless salem oggm-deps oggm; do
 	for os in linux osx; do
 		for CONDA_BUILD_PY in 3.5 3.6; do
 			echo "    - stage: $stage" >> .travis.yml
-			echo "      script: ./ci/travis_stage_script.sh $stage" >> .travis.yml
+			echo "      script: travis_wait 60 ./ci/travis_stage_script.sh $stage" >> .travis.yml
 			echo "      env: CONDA_BUILD_PY=$CONDA_BUILD_PY" >> .travis.yml
 			echo "      os: $os" >> .travis.yml
 		done
