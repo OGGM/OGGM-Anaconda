4d9402074a41167210c9a23f927154b93f377c6b
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2017-08-10 14:13:24 +0200

    Move more stuff into script

--- ci/travis_stage_script.sh
+++ ci/travis_stage_script.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-set -e
+set -xe
 cd "$(dirname "$0")/.."
 
 export MPLBACKEND=agg
 export CONDA_BLD_PATH="$PWD/conda-bld"
 unset TRAVIS
+
+rvm get head || true
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh; fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; fi
+bash miniconda.sh -b -p $HOME/miniconda
+rm miniconda.sh
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+
+conda config --set always_yes yes --set changeps1 no
+conda install -q -c conda-forge ca-certificates
+conda update -q --all
+conda update -q conda
+conda info -a
+conda install -q conda-build anaconda-client
+export SSL_CERT_FILE="$HOME/miniconda/ssl/cacert.pem"
 
 pushd build
 conda build --no-anaconda-upload --python "${CONDA_BUILD_PY}" --channel oggm --channel conda-forge --channel defaults --override-channels "$@"
