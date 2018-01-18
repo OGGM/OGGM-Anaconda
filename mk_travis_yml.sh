33a3faaa6ca9dfb1101687a9698ea9b0b10c34f4
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-01-18 16:12:34 +0100

    Do Linux Builds first, then OSX, since OSX takes forever and often fails

--- mk_travis_yml.sh
+++ mk_travis_yml.sh
@@ -6,10 +6,10 @@ cat .travis_head.yml > .travis.yml
 echo "jobs:" >> .travis.yml
 echo "  include:" >> .travis.yml
 
-for stage in motionless salem oggm-deps oggm; do
-	for os in linux osx; do
+for os in linux osx; do
+	for stage in motionless salem oggm-deps oggm; do
 		for CONDA_BUILD_PY in 3.5 3.6; do
-			echo "    - stage: $stage" >> .travis.yml
+			echo "    - stage: ${stage}_${os}" >> .travis.yml
 			echo "      script: travis_wait 60 ./ci/travis_stage_script.sh $stage" >> .travis.yml
 			echo "      env: CONDA_BUILD_PY=$CONDA_BUILD_PY" >> .travis.yml
 			echo "      os: $os" >> .travis.yml
