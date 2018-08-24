9480167bc6ca9043257902ca798d39e106833bf5
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-08-24 14:01:53 +0200

    Upload to anaconda after every step

--- build/build.cmd
+++ build/build.cmd
@@ -1,6 +1,7 @@
 setlocal
 cd %~dp0
 set CONDA_BLD_PATH=%cd%\..\conda-bld
+set ANACONDA_API_TOKEN=%ANACONDA_AUTH_TOKEN%
 
 call:cb %* .\motionless || exit /B 1
 call:cb %* .\salem || exit /B 1
@@ -12,8 +13,8 @@ exit /B %ERRORLEVEL%
 
 :cb
 if DEFINED CONDA_BUILD_PY (
-conda build --no-anaconda-upload --python %CONDA_BUILD_PY% --channel conda-forge --channel defaults --override-channels %* || exit /B 1
+conda build --python %CONDA_BUILD_PY% --channel conda-forge --channel defaults --override-channels %* || exit /B 1
 ) else (
-conda build --no-anaconda-upload --python 3.6 --channel conda-forge --channel defaults --override-channels %* || exit /B 1
+conda build --python 3.6 --channel conda-forge --channel defaults --override-channels %* || exit /B 1
 )
 exit /B 0
--- ci/anaconda_upload.cmd
+++
@@ -1,5 +0,0 @@
-setlocal
-cd %~dp0
-cd ..
-for /r %%f in (conda-bld\win-64\*.tar.bz2) do ( anaconda -t %ANACONDA_AUTH_TOKEN% upload -u oggm %%f )
-exit 0
