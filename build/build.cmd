c6478a717817c58ee3f70c9d0464311f7e418ec9
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-09-17 19:06:46 +0200

    Build pytest-mpl first

--- build/build.cmd
+++ build/build.cmd
@@ -3,9 +3,9 @@ cd %~dp0
 set CONDA_BLD_PATH=%cd%\..\conda-bld
 set ANACONDA_API_TOKEN=%ANACONDA_AUTH_TOKEN%
 
+call:cb %* .\pytest-mpl || exit /B 1
 call:cb %* .\motionless || exit /B 1
 call:cb %* .\salem || exit /B 1
-call:cb %* .\pytest-mpl || exit /B 1
 call:cb %* .\oggm-deps || exit /B 1
 call:cb %* .\oggm || exit /B 1
 
