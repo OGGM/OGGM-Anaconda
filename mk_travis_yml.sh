6f867b4e18fab1bcd9d6a9df11dd848b1ae884e8
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-09-18 12:20:28 +0200

    Don't separate stages by OS

--- mk_travis_yml.sh
+++ mk_travis_yml.sh
@@ -6,12 +6,12 @@ cat .travis_head.yml > .travis.yml
 for stage in motionless salem pytest-mpl oggm-deps oggm; do
 	for os in linux osx; do
 		for CONDA_BUILD_PY in 3.5 3.6 3.7; do
-			echo "    - stage: ${stage}_${os}" >> .travis.yml
+			echo "    - stage: ${stage}" >> .travis.yml
 			echo "      script: ./ci/travis_stage_script.sh $stage" >> .travis.yml
 			echo "      env: CONDA_BUILD_PY=$CONDA_BUILD_PY" >> .travis.yml
 			echo "      os: $os" >> .travis.yml
 			if [[ "$os" == "osx" ]]; then
-				echo "      osx_image: xcode9.1" >> .travis.yml
+				echo "      osx_image: xcode10" >> .travis.yml
 			fi
 		done
 	done
