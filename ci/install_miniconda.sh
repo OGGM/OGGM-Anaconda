67256837a0b73759ef7410604200f386049bf00b
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-11-13 12:18:54 +0100

    Factor out conda installation into its own file

---
+++ ci/install_miniconda.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+unset TRAVIS
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+	wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda.exe
+	chmod +x miniconda.exe
+	./miniconda.exe "/InstallationType=JustMe" "/AddToPath=0" "/RegisterPython=0" "/NoRegistry=1" "/S" "/D=$(cygpath -w -a $HOME/miniconda)"
+	rm miniconda.exe
+else
+	rvm get head || true
+	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh; fi
+	if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; fi
+	bash miniconda.sh -b -p $HOME/miniconda
+	rm miniconda.sh
+fi
+
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
