75ae0495a075b25003275412ad244d3127672a34
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2024-08-20 20:52:58 +0200

    Force latest conda-verify version

--- ci/setup-conda.sh
+++ ci/setup-conda.sh
@@ -8,8 +8,8 @@ if [[ "${RUNNER_OS}" != "Windows" ]]; then
     RQ sudo chown -R "$(id -u):$(id -g)" "$CONDA"
 fi
 
-RQ conda config --set always_yes yes --set changeps1 no --set channel_priority strict
+RQ conda config --set always_yes yes --set changeps1 no
 RQ conda update -q conda
 RQ conda update -c conda-forge -q --all
-RQ conda install -c conda-forge -q conda-build conda-verify anaconda-client
+RQ conda install -c conda-forge -q conda-build "conda-verify>=3" anaconda-client
 RQ conda info -a
